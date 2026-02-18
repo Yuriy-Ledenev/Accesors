95138e7a0501509193d7573b9b1186c6affefbbe
Yuriy Ledenev <90552258+Yuriy-Ledenev@users.noreply.github.com>
2026-02-18 17:23:29 +0300

    Add files via upload

---
+++ Nasledovaniye/Nasledovaniye/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasledovaniye
+{
+    class Person
+    {
+        public string name;
+        private int Age;
+        public int age;
+       
+        public Person(string name)
+        {
+            this.name = name;
+            Console.WriteLine("Срабатывание конструктора Person(string name)");
+        }
+        public Person(string name, int age) : this(name)
+        {
+            this.age = age;
+            Console.WriteLine("Срабатывание конструктора Person(string name, int age)");
+        }
+        public Person() { }
+    }
+}
---
+++ Nasledovaniye/Nasledovaniye/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Nasledovaniye
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee tom = new Employee("Tom", 22, "Microsoft");
+            Console.WriteLine(tom.name+" "+ tom.age + " "+tom.company);
+        }
+    }
+
+
+    
+   
+    }
+
