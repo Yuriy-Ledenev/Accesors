11f7ef6b2fc5ddcd26f1981cc57b6631a4528eae
Yuriy Ledenev <90552258+Yuriy-Ledenev@users.noreply.github.com>
2026-02-18 17:07:50 +0300

    Add files via upload

---
+++ Nasledovaniye/Nasledovaniye/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasledovaniye
+{
+    class Person
+    {
+        public string name;
+        private int Age;
+        public int age;
+       /* {
+           get
+            {
+                try { return this.Age;
+                }
+                catch { return 0; }
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    Age = 0;
+                    Console.WriteLine("Введён некорректный возраст, присвоено значение 0");
+                }
+                else
+                if (value >= 150)
+                {
+                    Age = 0;
+                    Console.WriteLine("Введён некорректный возраст, присвоено значение 0");
+                }
+                else { Age = value; }
+            }
+        }*/
+
+        public Person(string name)
+        {
+            this.name = name;
+            Console.WriteLine("Срабатывание конструктора Person(string name)");
+        }
+        public Person(string name, int age) : this(name)
+        {
+            this.age = age;
+            Console.WriteLine("Срабатывание конструктора Person(string name, int age)");
+        }
+        public Person() { }
+    }
+}
